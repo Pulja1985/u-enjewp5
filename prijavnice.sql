3b475f6de9d3dfa7550e9499b49f326885520fae
Pulja1985 <hpuljic85@gmail.com>
2024-05-25 19:28:38 +0200

    update prijavnice

--- prijavnice.sql
+++ prijavnice.sql
@@ -30,3 +30,46 @@
 --create table komentari(
 --napomena varchar(500)
 --);
+--use prijavnice
+--insert into vozaci values
+--(1001,'hrvoje', 'puljicilic' ,28104444467, 'ne', 'hpuljic85@gmail.com')
+--use prijavnice
+--drop table vozaci
+--drop table utrke
+--drop table vozila
+--drop table komentari
+--create table vozaci(
+--sifra int,
+--ime varchar(50),
+--prezime varchar(50),
+--oib char(11),
+--clankluba bit,
+--email varchar(100)
+--);
+--create table vozila(
+--marka varchar(50),
+--model varchar(50),
+--snaga int,
+--gorivobenzin bit,
+--gorivodizel bit,
+--elektricni bit,
+--turbo bit,
+--nitro bit,
+--pogon varchar(50)
+--);
+--create table utrke(
+--subota bit,
+--nedjelja bit,
+--streetrace bit,
+--proklasa bit,
+--dizel bit,
+--testandtune bit
+--);
+--create table komentari(
+--napomena varchar(500)
+--);
+insert into vozaci values
+--(1001,'hrvoje', 'puljicilic' ,28104444467, '0', 'hpuljic85@gmail.com')
+--(1002,'dominik', 'puljicilic' ,12345678901, '1', 'dominik92@gmai.com')
+--(1003,'slaven', 'krainovic', 12341234123,'1','slaven84@gmail.com')
+(1004, 'mato', 'pastulovic', 12345123451, '1', 'mato86@gmail.com')
