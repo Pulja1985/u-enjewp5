952d3a960c1113ab090f7c913a061320fc190e7c
Pulja1985 <hpuljic85@gmail.com>
2024-06-05 16:09:13 +0200

    Update prijavnice1.sql

--- prijavnice1.sql
+++ prijavnice1.sql
@@ -33,6 +33,31 @@ alter table vozila_utrke add foreign key(vozilo_sifra)references vozila(sifra);
 alter table vozila add foreign key(vozaci_sifra)references vozaci(sifra);
 alter table vozila_utrke add foreign key (utrke_sifra)references utrke(sifra);
 
+select * from vozaci;
+
+insert into vozaci(ime,prezime,oib)values
+('hrvoje','puljic-ilic',24104444467),
+('dominik','puljic-ilic',12345678901),
+('matija','puljic-ilic',23456789012),
+('slaven','krainovic',34567890123)
+
+select * from vozila;
+
+insert into vozila(marka,model,snaga,pogon,vozaci_sifra)values
+('honda','civic_ek4',160,'prednji',1),
+('honda','civic_ej2',185,'prednji',2),
+('honda','accord_cl8',155,'prednji',3),
+('honda','civic_eg4',150,'prednji',4)
+
+select * from utrke;
+
+insert into utrke(datum,mjesto,naziv)values
+('2024-06-01 09-00','osijek','osrs31'),
+('2024-06-01 09-00','osijek','osrs31'),
+('2024-06-01 09-00','osijek','osrs31'),
+('2024-06-01 09-00','osijek','osrs31')
+
+
 
 --insert into vozaci values
 --(1001,'hrvoje', 'puljicilic' ,28104444467, 'ne', 'hpuljic85@gmail.com')
@@ -46,7 +71,7 @@ alter table vozila_utrke add foreign key (utrke_sifra)references utrke(sifra);
 --insert into vozila values
 --(1001, 'honda', 'civic', 500, 1, 0, 0, 1, 0, 'prednji'),
 --(1002, 'honda', 'civic', 185, 1, 0, 0, 0, 0, 'prednji'),
---(1003, 'honda', 'civic', 320, 1, 0, 0, 1, 0, 'prednji'),
+--(1003, 'honda', 'civic', 320, 1, 0, 0, 1, 0, 'prednji'),hrvoje
 --(1004, 'honda', 'civic', 300, 1, 0, 0, 1, 0, 'prednji')
 
 --insert into utrke values
